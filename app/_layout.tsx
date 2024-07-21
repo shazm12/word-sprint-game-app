8b66971c26a57d54947f40d34802cbd6a8d8f74e
shazm12 <shamik.bera2019@vitstudent.ac.in>
2024-07-22 03:45:00 +0530

    chore: formatted the file;

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { Stack } from 'expo-router/stack';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import LinearGradBg from './components/LinearGradBg';
-import game from './game';
+import React from "react";
+import { Stack } from "expo-router/stack";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { UserProvider } from "./contexts/userContext";
 
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-
-    >
-      <Stack.Screen name="game"options={{ animation:"fade", gestureEnabled: false, headerLeft: () => null }} />  
-    </Stack>
+      <UserProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen
+            name="game"
+            options={{
+              animation: "fade",
+              gestureEnabled: false,
+              headerLeft: () => null,
+            }}
+          />
+        </Stack>
+      </UserProvider>
     </GestureHandlerRootView>
   );
 }
